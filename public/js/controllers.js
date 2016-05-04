d3b9679f135f60e86e2ff6b40aeff41bcb18ecec
ypyang237 <ypyang237@gmail.com>
2016-05-04 11:24:50 -1000

    adds main character in values.js and injects in scope through controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,11 +1,16 @@
   'use strict';
 
-  myApp.controller('myController', ['$scope', function($scope) {
+var myApp = angular.module('myApp');
+
+  myApp.controller('myController', ['$scope', 'MainCharacter', function($scope, MainCharacter) {
     //attach model to scope
     $scope.myFirstName = "Pam";
     //applies a new model to the scope
     //scope is myFirstName
 
     $scope.myModel = "Mochi";
+
+    $scope.mainCharacter = MainCharacter;
+
 
   }]);
