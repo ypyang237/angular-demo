9ceb4aa6954eebf4f5f6f6e46b8eb706ccc09402
ypyang237 <ypyang237@gmail.com>
2016-05-04 10:50:34 -1000

    initialize new angular module

---
+++ public/js/myApp.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//creating uses a 2nd array argument to import dependencies
+angular.module('myApp', []); //creates a module named myApp
+
+// retrieval has only one argument
+var myApp = angular.module('myApp'); //retrieve the module named myApp
+
+myApp
+  .config(function() {
+    //config
+
+  })
+  .run(function() {
+    //initialize
+
+  });
