c5262e4dbb9c565d761b577a7929508636baa9ea
ypyang237 <ypyang237@gmail.com>
2016-05-04 11:08:45 -1000

    add myModel to myController

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -16,9 +16,9 @@ myApp
 
   });
 
-  myApp.controller('myController', function($scope) {
+  myApp.controller('myController', ['$scope', function($scope) {
     //attach model to scope
     $scope.myFirstName = "Pam";
     //applies a new model to the scope
     //scope is myFirstName
-  })
+  }])
